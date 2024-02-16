c7d91d972a8bc9f37056dc1a4efe25f40e2e50d6
sucs28mee <adamdudkiewicz2003@gmail.com>
2024-02-16 01:37:10 +0100

    Added boring argument.

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,9 @@ mod parser;
 struct Args {
     /// Expression to be evaluated (optional)
     expression: Option<String>,
+    /// Display output without colors
+    #[arg(short, long, default_value_t = false)]
+    boring: bool,
 }
 
 fn main() -> Result<(), Box<dyn error::Error>> {
@@ -32,9 +35,24 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         return Ok(());
     }
 
-    println!("| \x1b[90mInput a math expression (\"q\" to exit).\x1b[0m");
+    println!(
+        "{}",
+        if args.boring {
+            "| Input a math expression (\"q\" to exit)."
+        } else {
+            "| \x1b[90mInput a math expression (\"q\" to exit).\x1b[0m"
+        }
+    );
+
     loop {
-        print!("\x1b[96m| > \x1b[0m");
+        print!(
+            "{}",
+            if args.boring {
+                "| > "
+            } else {
+                "\x1b[96m| > \x1b[0m"
+            }
+        );
         io::stdout().flush()?;
 
         let expr = {
@@ -54,10 +72,17 @@ fn main() -> Result<(), Box<dyn error::Error>> {
                     println!("\x1b[94m|\x1b[0m {:?}", ast);
                 }
 
-                println!("\x1b[94m| =\x1b[0m {}\n", ast.evaluate())
+                println!(
+                    "{}",
+                    if args.boring {
+                        format!("| = {}\n", ast.evaluate())
+                    } else {
+                        format!("\x1b[94m| =\x1b[0m {}\n", ast.evaluate())
+                    }
+                )
             }
             Err(err) => {
-                print!("\x1b[93m    ");
+                print!("{}", if args.boring { "    " } else { "\x1b[93m    " });
                 for _ in 0..err.0 {
                     print!(" ");
                 }
@@ -66,7 +91,14 @@ fn main() -> Result<(), Box<dyn error::Error>> {
                     print!("^");
                 }
 
-                println!("\n|\x1b[0m {}", err.1);
+                println!(
+                    "{}",
+                    if args.boring {
+                        format!("\n| {}", err.1)
+                    } else {
+                        format!("\n|\x1b[0m {}", err.1)
+                    }
+                );
             }
         }
     }
